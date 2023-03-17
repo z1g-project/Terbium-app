4feefd4784888d3d676d1ed23ff8157e15443673
Notplayingallday383 <72810050+Notplayingallday383@users.noreply.github.com>
2023-03-16 20:10:34 -0400

    Open Sourced

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Terbium-app
 The Official Terbium WebOS Chrome App!
+
+the source code will be added soon so bear with me while I package it and hopefully am able to upload it all to this repo!
+
+(C) Copyright 2021-2023 z1g Project
---
+++ config.js
@@ -0,0 +1,9 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Terbium",
+   "version": "1.45-beta",
+   "homepage": "https://terbium-replit.johnglynn2.repl.co",
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
